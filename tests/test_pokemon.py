302b04d7e2513d7453aa47907f097f9470ef7aa0
Jay Newey <jayluxray@gmail.com>
2022-01-08 19:52:44 +0000

    test: add test for `Pokemon.name_at()`

--- tests/test_pokemon.py
+++ tests/test_pokemon.py
@@ -7,6 +7,16 @@ def test_name():
     assert mock_unite_db.pokemon[0].name == "Blastoise"
 
 
+def test_name_at():
+    assert mock_unite_db.pokemon[0].name_at(0) == "Squirtle"
+    assert mock_unite_db.pokemon[0].name_at(1) == "Squirtle"
+
+    assert mock_unite_db.pokemon[0].name_at(5) == "Wartortle"
+
+    assert mock_unite_db.pokemon[0].name_at(9) == "Blastoise"
+    assert mock_unite_db.pokemon[0].name_at(16) == "Blastoise"
+
+
 def test_stats():
     # using indexes
     assert mock_unite_db.pokemon[0].stats[0].hp == 3225
